9f196b3ce4ad72e4f67b12dbf394690f4c024941
Gabriel Alves <gabriel52.alves@gmail.com>
2025-01-11 18:13:32 -0300

    feat: enhance send_invoice function to include due date and barcode in WhatsApp messages

--- src/faturas_whatsapp/services/pdf_extractor.py
+++ src/faturas_whatsapp/services/pdf_extractor.py
@@ -1,44 +1,46 @@
 import fitz
 import re
 from services.downloader import InvoiceDownloader
-import os
+
 class InvoiceExtractor:
-    def __init__(self):
+    def __init__(self, unit_consumption, cpf, date_birth):
+        self.unit_consumption, self.cpf, self.date_birth = unit_consumption, cpf, date_birth
         downloader = InvoiceDownloader()
         self.pdf_path = downloader.get_invoice(
-            os.getenv('UNIT_CONSUMPTION'), 
-            os.getenv('CPF'), 
-            os.getenv('DATE_BIRTH')
+            self.unit_consumption, self.cpf, self.date_birth
         )
         self.text = self._extract_text()
 
     def _extract_text(self):
-        # Extrai o texto de todas as páginas do PDF.
         document = fitz.open(self.pdf_path)
         return "".join(page.get_text() for page in document)
 
     def get_total_to_pay(self):
-        # Extrai o total a pagar da fatura
         pattern = r"Total a Pagar \(R\$.*?\n(\d{1,3}(?:\.\d{3})*,\d{2})"
         match = re.search(pattern, self.text, re.DOTALL)
         return match.group(1) if match else None
 
     def get_reference_month(self):
-        # Extrai o mês de referência da fatura.
         pattern = r"Referência\s*(\d{2}/\d{4})"
         match = re.search(pattern, self.text)
         return match.group(1) if match else None
     
     def get_holder_name(self):
-        # Extrai o nome do titular da fatura.
-        pattern = r"Pagador:\s*(.*)"
+        pattern = r"([^\n]+)\s+Pagador:"
         match = re.search(pattern, self.text)
         return match.group(1).strip() if match else None
 
     def get_address(self):
-        # Extrai o endereço da fatura.
-        pattern = r"Endereço:\s*(.*)"
+        pattern = r"([^\n]+)\s+Endereço:"
         match = re.search(pattern, self.text)
         return match.group(1).strip() if match else None
 
-
+    def get_due_date(self):
+        pattern = r"Vencimento\s+(\d{2}/\d{2}/\d{4})"
+        match = re.search(pattern, self.text)
+        return match.group(1) if match else None
+    
+    def get_barcode(self):
+        pattern = r"(\d{5}\.\d{5}\s*\d{5}\.\d{6}\s*\d{5}\.\d{6}\s*\d\s*\d{14})"
+        match = re.search(pattern, self.text, re.DOTALL)
+        return match.group(1).replace(" ", "") if match else None
--- src/faturas_whatsapp/services/send_whatsapp.py
+++ src/faturas_whatsapp/services/send_whatsapp.py
@@ -1,14 +1,25 @@
-from services.printer import print_invoice
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import os
 
-def send_invoice(contact_name, holder, address, consumer_unit, month, total_to_pay):
-    driver = webdriver.Chrome()  
+def send_invoice(contact_name, holder, address, consumer_unit, month, due_date, total_to_pay, barcode):
+    profile_path = os.path.join(os.getcwd(), "chrome_profile")
+
+    # Configuração das opções do Chrome
+    options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    options.add_argument(f"user-data-dir={profile_path}") 
+    options.add_argument("--profile-directory=Default")
+    
+    driver = webdriver.Chrome(options=options)
     driver.get("https://web.whatsapp.com/")
+    
     print("Escaneie o QR code para fazer login...")
     time.sleep(3)
     
@@ -21,7 +32,7 @@ def send_invoice(contact_name, holder, address, consumer_unit, month, total_to_p
         contact_element.click()  
         print(f"Contato {contact_name} encontrado.")
 
-        messages = [f"Olá, *{contact_name}*!", f"A fatura de *{holder}* na *CELESC* está disponível", f"A cobrança é referente ao mês *{month}* no endereço *{address}*, unidade consumidora *{consumer_unit}*.", f"O total a pagar é *R${total_to_pay}*."]
+        messages = [f"Olá, *{contact_name}*!", f"A fatura de *{holder}* na *CELESC* está disponível", f"A cobrança é referente ao mês *{month}* no endereço *{address}*, unidade consumidora *{consumer_unit}*.", f"O total a pagar é *R${total_to_pay}*. O vencimento do boleto é em *{due_date}*." , f"Segue o código de barras para pagamento: {barcode}"]
 
         message_box = WebDriverWait(driver, 30).until(
             EC.presence_of_element_located((By.XPATH, '//div[@contenteditable="true" and @data-tab="10"]')) # Local do input de mensagem 
