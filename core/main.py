3f02ea5d065b031a2f32387d203caf051e895b6f
Gabriel Alves <gabriel52.alves@gmail.com>
2025-01-13 08:25:42 -0300

    chore: reorganizing file structure to improve clarity

--- core/main.py
+++ core/main.py
@@ -1,13 +1,9 @@
 from services.pdf_extractor import InvoiceExtractor
 from services.send_whatsapp import send_invoice
 from services.get_user_data import get_user_data
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 if __name__ == "__main__": 
-    unit_consumption, cpf, date_birth = get_user_data()
+    unit_consumption, cpf, date_birth, contact_to_send_message = get_user_data()
 
     invoice_details = InvoiceExtractor(unit_consumption, cpf, date_birth)
 
@@ -17,10 +13,8 @@ if __name__ == "__main__":
     address = invoice_details.get_address()
     due_date = invoice_details.get_due_date()
     barcode = invoice_details.get_barcode()
-
     # print(invoice_details._extract_text())
 
     print("Enviando fatura via WhatsApp...")
 
-    contact_name = input('Digite o nome do contato: ')
-    send_invoice(contact_name, holder, address, unit_consumption, month, due_date, total_to_pay, barcode)
+    send_invoice(contact_to_send_message, holder, address, unit_consumption, month, due_date, total_to_pay, barcode)
--- core/services/get_user_data.py
+++ core/services/get_user_data.py
@@ -2,20 +2,21 @@ from dotenv import load_dotenv, set_key
 import os
 
 def get_user_data():
-    load_dotenv()  # Carregar variáveis de ambiente do arquivo .env
+    load_dotenv()  
 
     if not os.getenv('UNIT_CONSUMPTION') or not os.getenv('CPF') or not os.getenv('DATE_BIRTH'):
         unit_consumption = input('Digite a unidade consumidora (Ex.: 1234566789): ')
         cpf = input('Digite o CPF (Ex.: 000000000-00): ')
         date_birth = input('Digite a data de nascimento (Ex.: DD/MM/AAAA): ')
+        contact_to_send_message = input('Digite o nome do contato para enviar a fatura (Ex: John Doe): ')
 
         set_key('.env', 'UNIT_CONSUMPTION', unit_consumption)
         set_key('.env', 'CPF', cpf)
         set_key('.env', 'DATE_BIRTH', date_birth)
-        
+
     else:
         unit_consumption = os.getenv('UNIT_CONSUMPTION')
         cpf = os.getenv('CPF')
         date_birth = os.getenv('DATE_BIRTH')
 
-    return unit_consumption, cpf, date_birth
+    return unit_consumption, cpf, date_birth, contact_to_send_message
